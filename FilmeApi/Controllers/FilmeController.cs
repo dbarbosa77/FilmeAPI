1af74379dff9e33b6d8c17376b508fcdb400b5b4
Douglas Rodrigues Barbosa <drbdodo@gmail.com>
2023-08-16 20:59:56 -0300

    incluso get e post

--- FilmeApi/Controllers/FilmeController.cs
+++ FilmeApi/Controllers/FilmeController.cs
@@ -8,16 +8,31 @@ namespace FilmeApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 1;
+
 
     [HttpPost]
-    public void AdicionaFilme([FromBody]Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine("Feito a inclusão com sucesso.");
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
-        Console.WriteLine(filme.Genero);
-        Console.WriteLine(filme.Id);
+        Console.WriteLine($"Feito a inclusão do filme {filme.Titulo} de id {filme.Id} com sucesso.");
+        return CreatedAtAction(nameof(RecuperaFilme), new { id = filme.Id }, filme);
 
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> ListarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+
+    public IActionResult RecuperaFilme(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
--- FilmeApi/Models/Filme.cs
+++ FilmeApi/Models/Filme.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmeApi.Models;
 
 public class Filme
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage ="O titulo do filme é obrigatório")]
     public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(70,600,ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
+
+    [Required(ErrorMessage = "A nota do filme é obrigatório")]
     public double Nota { get;set; }
 }
